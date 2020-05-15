8202d9c01f02b7071b284b4998249364332237c7
Pranav Jain <pranav19sept@gmail.com>
2020-05-15 10:10:45 +0530

    Enabled switching between light and dark ui

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -6,12 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
+  dark: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
-    // document.documentElement.style.getPropertyValue
-    // document.documentElement.style.setProperty
   }
 
+  toggleTheme() {
+    this.dark = !this.dark;
+    let theme = this.dark ? 'dark' : 'light';
+    let properties: string[] = [
+      '--ui-primary',
+      '--ui-secondary',
+      '--ui-pre-accent',
+      '--ui-color',
+      '--ui-shadow-color'
+    ]
+    for (let property of properties) {
+      let value = getComputedStyle(document.documentElement).getPropertyValue(property+'-'+theme);
+      document.documentElement.style.setProperty(property, value);
+    }
+  }
 }
